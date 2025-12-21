ed8b102edf121ebe425014dd57b937b1459b226b
= <=>
2025-12-21 12:48:29 +0530

    updated

--- app/index.js
+++ app/index.js
@@ -3,9 +3,13 @@ const app = express();
 const https = require('https');
 const fs = require('fs');
 const path = require('path');
+// const options = {
+//     key: fs.readFileSync('./ssl/private.pem'),
+//     cert: fs.readFileSync('./ssl/certificate.pem')
+// }
 const options = {
-    key: fs.readFileSync('./ssl/private.pem'),
-    cert: fs.readFileSync('./ssl/certificate.pem')
-}
+    key: fs.readFileSync('/etc/letsencrypt/live/frush.franticpro.com/privkey.pem', 'utf-8'),
+    cert: fs.readFileSync('/etc/letsencrypt/live/frush.franticpro.com/fullchain.pem', 'utf-8'),
+};
 const server = https.createServer(options, app);
 module.exports = { app, server }
