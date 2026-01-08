f91813a4d24a3e90e73d74aec5216d237e4a19c9
frantic03117 <shahrukh@franticpro.com>
2026-01-08 13:46:20 +0530

    slash added

--- routes/AppRoutes.js
+++ routes/AppRoutes.js
@@ -4,5 +4,5 @@ const { admin_login } = require("../src/controller/userController");
 const { cashfreeWebhook, dashboard } = require("../src/controller/OrderController");
 router.post('/login', admin_login);
 router.post('/webhook', express.raw({ type: 'application/json' }), cashfreeWebhook);
-router.get('dashboard', dashboard)
+router.get('/dashboard', dashboard)
 module.exports = router;
