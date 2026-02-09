1fd5b12bc5eb342bf354e6e441f1672a029f481d
frantic03117 <shahrukh@franticpro.com>
2026-02-09 13:32:06 +0530

    https to http

--- app/index.js
+++ app/index.js
@@ -1,15 +1,16 @@
 const express = require('express')
 const app = express();
 const https = require('https');
+const http = require('http');
 const fs = require('fs');
 const path = require('path');
-const options = {
-    key: fs.readFileSync('./ssl/private.pem'),
-    cert: fs.readFileSync('./ssl/certificate.pem')
-}
 // const options = {
-//     key: fs.readFileSync('/etc/letsencrypt/live/frush.franticpro.com/privkey.pem', 'utf-8'),
-//     cert: fs.readFileSync('/etc/letsencrypt/live/frush.franticpro.com/fullchain.pem', 'utf-8'),
-// };
-const server = https.createServer(options, app);
+//     key: fs.readFileSync('./ssl/private.pem'),
+//     cert: fs.readFileSync('./ssl/certificate.pem')
+// }
+/*const options = {
+    key: fs.readFileSync('/etc/letsencrypt/live/frush.franticpro.com/privkey.pem', 'utf-8'),
+    cert: fs.readFileSync('/etc/letsencrypt/live/frush.franticpro.com/fullchain.pem', 'utf-8'),
+};*/
+const server = http.createServer(app);
 module.exports = { app, server }
