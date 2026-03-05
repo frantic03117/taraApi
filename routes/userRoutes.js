fd5cd9ddca77351f49940adbc03f09ed88fde503
frantic03117 <shahrukh@franticpro.com>
2026-03-05 14:14:30 +0530

    profile added

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,12 +1,13 @@
 const { Router } = require("express");
 const { Auth } = require("../src/middleware/Auth");
 const Store = require("../src/middleware/Store");
-const { send_otp, verify_otp, store_profile, update_profile, user_list } = require("../src/controller/userController");
+const { send_otp, verify_otp, store_profile, update_profile, user_list, my_profile } = require("../src/controller/userController");
 const { list_orders } = require("../src/controller/OrderController");
 
 const router = Router();
 router.post('/send-otp', send_otp);
 router.get('/', Auth(), user_list);
+router.get('/my_profile', Auth(), my_profile);
 router.post('/verify-otp', verify_otp);
 router.get('/order', Auth('User'), list_orders);
 router.post('/register', Store('image').fields([
