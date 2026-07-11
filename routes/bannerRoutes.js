0fb0bb1220298e657da48a004764c99245196cd6
= <=>
2026-07-11 18:55:23 +0530

    updated

--- routes/bannerRoutes.js
+++ routes/bannerRoutes.js
@@ -8,6 +8,6 @@ router.post('/', Auth('Admin'), Store('any').single('image'), _create);
 router.put('/update/:id', Auth('Admin'), update_banner);
 router.delete('/:id', Auth('Admin'), delete_banner);
 router.get('/test', (req, res) => {
-    return res.json({success : 1, data : "CI CD works"});
+    return res.json({success : 1, data : "CI CD works change did it changes"});
 })
 module.exports = router;
